0728b8b6b6466277f2837972001fbba51125ab86
Chris Coe <chrcoe@ieee.org>
2015-08-05 19:36:43 -0400

    updated for SQLAlchemy changes

--- flaskapp/manage.py
+++ flaskapp/manage.py
@@ -1,18 +1,30 @@
 #! /usr/bin/env python
 import os
 
-from flask.ext.script import Manager, Shell
+from flask.ext.script import Manager, Shell, Command
 
-from flaskapp import create_app
+from flaskapp import create_app, db
+from flaskapp.models import Cookie
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 
 
 def make_shell_context():
-    return dict(app=app)
+    return dict(app=app, db=db)
+
+
+class DBInit(Command):
+    ''' Creates tables from SQLAlchemy models. '''
+
+    def __init__(self, db):
+        self.db = db
+
+    def run(self):
+        self.db.create_all()
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
+manager.add_command('db_init', DBInit(db))
 
 if __name__ == '__main__':
     manager.run()
