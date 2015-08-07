0f9e5c55745a1ee5c186b4032558ac600b8860ae
Chris Coe <chrcoe@ieee.org>
2015-08-07 13:08:32 -0400

    update is done, next up is unit testing for flask environment

--- flaskapp/api/models.py
+++ flaskapp/api/models.py
@@ -8,3 +8,8 @@ class Cookie(db.Model):
     cookie_name = db.Column(db.String(50), index=True)
     cookie_recipe_url = db.Column(db.String(255))
     quantity = db.Column(db.Integer())
+
+    def __init__(self, cookie_name=None, cookie_recipe_url=None, quantity=0):
+        self.cookie_name = cookie_name
+        self.cookie_recipe_url = cookie_recipe_url
+        self.quantity = quantity
--- flaskapp/api/resources/cookie.py
+++ flaskapp/api/resources/cookie.py
@@ -33,12 +33,12 @@ class CookieListAPI(Resource):
     @marshal_with(cookie_fields)
     def post(self):
         args = self.reqparse.parse_args()
-        cookie = Cookie()
-        cookie.cookie_name = args['cookie_name']
-        cookie.cookie_recipe_url = args['cookie_recipe_url']
-        cookie.quantity = args['quantity']
+        cookie = Cookie(
+            cookie_name=args['cookie_name'],
+            cookie_recipe_url=args['cookie_recipe_url'],
+            quantity=args['quantity']
+        )
         db.session.add(cookie)
-        # print(cookie.cookie_id)
         db.session.commit()
         return cookie
 
@@ -56,7 +56,7 @@ class CookieAPI(Resource):
         self.reqparse.add_argument('cookie_name', type=str, location='json')
         self.reqparse.add_argument(
             'cookie_recipe_url', type=str, location='json')
-        self.reqparse.add_argument('quantity', type=bool, location='json')
+        self.reqparse.add_argument('quantity', type=int, location='json')
         super(CookieAPI, self).__init__()
 
     @marshal_with(cookie_fields, envelope='cookie')
@@ -66,11 +66,14 @@ class CookieAPI(Resource):
     @marshal_with(cookie_fields, envelope='cookie')
     def put(self, cookie_id):
         cookie = Cookie.query.get_or_404(cookie_id)
-        if len(cookie) == 0:
-            abort(404)
         args = self.reqparse.parse_args()
-        print(args)
-        # TODO: finish the UPDATE method
+        if args['cookie_name']:
+            cookie.cookie_name = args['cookie_name']
+        if args['cookie_recipe_url']:
+            cookie.cookie_recipe_url = args['cookie_recipe_url']
+        if args['quantity']:
+            cookie.quantity = args['quantity']
+        db.session.commit()
         return cookie
 
     def delete(self, cookie_id):
